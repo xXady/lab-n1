5a9f2c17b1b30bb7c62435ffda4a8603db1c4f23
xXady <102038515+xXady@users.noreply.github.com>
2023-09-29 20:30:34 +0500

    Add files via upload

---
+++ Фт-220007-Худяков.py
@@ -0,0 +1,25 @@
+a = int(input())
+
+b = int(input())
+
+a1 = abs(a)
+
+b1 = abs(b)
+
+if a1 > b1:
+    
+    m = a1 - b1
+
+else:
+
+    m = b1 - a1
+
+print(a + b)
+
+print(a - b)
+
+print(a * b)
+
+print((a+b)/2)
+
+print(m)
